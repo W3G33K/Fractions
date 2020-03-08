5d4f8af334ff3d2820ea2d147ab72f375d17460d
Ryan K. Clark <mail.w3geek@gmail.com>
2020-03-07 23:30:04 -0500

    Converts a fraction into a decimal.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,31 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, world!" << endl;
+	int numerator = 0,
+		denominator = 0;
+	float result = 0.0f;
+
+	/*
+		TODO: Check for zero value;
+	*/
+	cout << "Type fraction numberator and press [Enter] key (must not be zero): ";
+	cin >> numerator;
+	cout << endl;
+
+	/*
+		TODO: Check for zero value;
+	*/
+	cout << "Type fraction denominator and press [Enter] key (must not be zero): ";
+	cin >> denominator;
+	cout << endl;
+
+	// TODO: Simplify and show steps
+	cout << "Converting fraction " << numerator << "/" << denominator << " to decimal ...";
+	result = ((float) numerator / denominator);
+	cout << endl;
+
+	cout << "Result: " << result;
+	cout << endl << endl;
 
 	return 0;
 }
